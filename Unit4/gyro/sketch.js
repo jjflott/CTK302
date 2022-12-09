e94f999a8d68044dfe8935fe1b486b3edad973ba
jjflott <jjflott@ilstu.edu>
2022-12-09 02:16:34 -0600

    update

--- Unit4/gyro/sketch.js
+++ Unit4/gyro/sketch.js
@@ -4,7 +4,8 @@
 Make sure you turn on orientation lock on your iPhone or Android device. */
 
 let alpha = 0, beta = 0 , gamma = 0; // gyroscope variablers
-let bunnyImage;
+let img1;
+let f1;
 let xPosition = 0;
 let yPosition = 0;
 let x = 0, y = 0, z = 0 ; // accelerometer data
@@ -16,7 +17,8 @@ function setup() {
 
 
 
-  bunnyImage = loadImage("assets/bunny.jpg");
+  img1 = loadImage("assets/itzay.jpeg");
+  f1 = loadFont("assets/smile.otf");
   imageMode(CENTER);
   rectMode(CENTER);
 
@@ -24,7 +26,7 @@ function setup() {
 
 function draw() {
 
-  background('#c6f5ff'); // light blue
+  background('lightpink'); // light blue
 
   // the map command !!!!
   // takes your variable and maps it from range 1 to range 2
@@ -38,33 +40,34 @@ function draw() {
 
   rotate(radians(alpha)); // rotate the bunny depending on the alpha intake
 
-  image(bunnyImage, 0, 0, 500, 500);
+  image(img1, 0, 100, 800, 600);
   // rect(0, 0, 100, 100) ;
   pop();
 
 
   // Text commands that display debugging data
-  textAlign(LEFT);
-  textSize(20);
-  fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y.toFixed(2), 25, 170);
-  text("z = " + z.toFixed(4), 25, 190);
+  // textAlign(LEFT);
+  // textSize(20);
+ // fill('black');
+  // text("orientation data:", 25, 25);
+  // textSize(15);
+  // text("alpha: " + alpha, 25, 50);
+  // text("beta: " + beta, 25, 70);
+  // text("gamma: " + gamma, 25, 90);
+  // textSize(20);
+  // text("acceleration data:", 25, 125);
+  // textSize(15);
+ //  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  // text("y = " + y.toFixed(2), 25, 170);
+  // text("z = " + z.toFixed(4), 25, 190);
 
   // Text that makes CTK type in the background
   fill('white');
   noStroke();
   textSize(300);
   textAlign(CENTER);
-  text("ctk", width / 2, height / 2);
+  textFont(f1, 50);
+  text("itzy :)", 600, 100);
 
 }
 
